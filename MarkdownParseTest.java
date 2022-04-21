ade246c270fb71074e8dbe1f0ad10555628fc174
jadenbanawa <jkbanawa@ucsd.edu>
2022-04-21 14:53:54 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,11 +13,61 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void markdownTest() {
+    public void markdownTest1() {
         List thisList = List.of("https://something.com", "some-thing.html");
 
         MarkdownParse mpObj = new MarkdownParse();
         Path filePath = Path.of("test-file.md");
+        try{
+
+        String fileContent = Files.readString(filePath);
+        ArrayList<String> outputList = mpObj.getLinks(fileContent);
+        assertEquals(thisList, outputList);
+
+        }catch(IOException e){
+
+        }
+ 
+    }
+
+    @Test
+    public void markdownTest2() {
+        List thisList = List.of( "https://github.com/jadenbanawa/markdown-parser");
+
+        MarkdownParse mpObj = new MarkdownParse();
+        Path filePath = Path.of("test-file2.md");
+        try{
+
+        String fileContent = Files.readString(filePath);
+        ArrayList<String> outputList = mpObj.getLinks(fileContent);
+        assertEquals(thisList, outputList);
+
+        }catch(IOException e){
+
+        }
+ 
+    }
+    @Test
+    public void markdownTest3() {
+        MarkdownParse mpObj = new MarkdownParse();
+        Path filePath = Path.of("test-file3.md");
+        try{
+
+        String fileContent = Files.readString(filePath);
+        ArrayList<String> outputList = mpObj.getLinks(fileContent);
+        assertEquals(null, outputList);
+
+        }catch(IOException e){
+
+        }
+ 
+    }
+    @Test
+    public void markdownTest4() {
+        List thisList = List.of("link.com", "link.com");
+
+        MarkdownParse mpObj = new MarkdownParse();
+        Path filePath = Path.of("test-file4.md");
         try{
 
         String fileContent = Files.readString(filePath);
